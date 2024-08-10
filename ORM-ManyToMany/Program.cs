87f3f8f39b366a505a1c2742dd272d4a0e7d0c6f
RizvanVeliyev <veliyevrizvan250@gmail.com>
2024-08-10 19:17:18 +0400

    Add project files.

---
+++ ORM-ManyToMany/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ORM_ManyToMany
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
